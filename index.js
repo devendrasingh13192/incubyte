0f418964437ca14dfcae3ee7ea6a7fb6d0416df5
devendrasingh13192 <dsinghtanwar19@gmail.com>
2024-10-18 10:39:07 +0530

    Support different delimiters

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 class addNumbers {
-     // Method to add numbers from a string
+    // Method to add numbers from a string with support for custom delimiters
   add(numbers) {
     if (numbers === "") {
       return 0; // Return 0 if the string is empty
     }
 
-    // Split the string by commas or new lines and convert each to a number
-    const numArray = numbers.split(/[\n,]+/).map(Number);
+    let delimiter = /[\n,]+/; // Default delimiters are commas and new lines
+
+    // Check if a custom delimiter is provided at the start of the string
+    if (numbers.startsWith("//")) {
+      // Extract the custom delimiter
+      const delimiterEndIndex = numbers.indexOf("\n");
+      delimiter = new RegExp(numbers.substring(2, delimiterEndIndex)); // Create a regex for the custom delimiter
+      numbers = numbers.substring(delimiterEndIndex + 1); // Remove the delimiter line from the string
+    }
+
+    // Split the string by the custom delimiter (or default ones) and convert to numbers
+    const numArray = numbers.split(delimiter).map(Number);
 
     // Sum the numbers
     return numArray.reduce((sum, num) => sum + num, 0);
